4853bec300d5ff41aabe86652bbf54b82d7fbf0a
God of Thunder <God of Thunder@GodofThunder-PC>
2021-07-19 21:44:35 +0530

    First Commit

---
+++ Practice Test/src/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The First Name:");
+		String str1=sc.next();
+		System.out.println("Enter the Last Name:");
+		String str2=sc.next();
+		System.out.println("Hello \n"+str1+" "+str2);
+	}
+}
