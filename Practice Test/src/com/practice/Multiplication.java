dcd2b58191fceae7303f67dd737649e609cdb175
God of Thunder <God of Thunder@GodofThunder-PC>
2021-07-19 22:21:51 +0530

    added multiplication

---
+++ Practice Test/src/com/practice/Multiplication.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class Multiplication {
+	
+	public static void main (String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the First Number:");
+		int num1=sc.nextInt();
+		System.out.println("Enter the Second Number:");
+		int num2=sc.nextInt();
+		System.out.println(num1+"*"+num2+" = "+num1*num2);
+	}
+
+}
